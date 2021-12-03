9e9362011254a806a88e51f6f15587fddddfc8f4
khalil DupGames <85350190+khalilDup@users.noreply.github.com>
2021-12-03 19:17:56 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Checkerboard_Pattern_of_Asterisks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+               Write an app that uses only the output statements
+               Console.Write( "* " );
+               Console.Write( " " );
+               Console.WriteLine();
+               to display the checkerboard pattern. A Console.WriteLine method call with no arguments outputs a single newline character. 
+               [Hint: Repetition statements are required.]
+
+             */
+
+            int Length = 16;
+            int Height = 8;
+
+            for (int i = 0; i <= Height; i++)
+            {
+                if (i % 2 == 0)
+                    Console.Write("* ");
+                else
+                    Console.Write("");
+
+                for (int j = 0; j <= Length; j++)
+                {
+                    if (j % 2 == 0)
+                        Console.Write(" ");
+                    else
+                        Console.Write("* ");
+                }
+                Console.WriteLine("");
+            }
+        }
+    }
+}
